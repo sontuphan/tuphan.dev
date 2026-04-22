bd9388844a7200ae5ecf37c874e820df0be5411d
Tu Phan <phan.son.tu.1994@gmail.com>
2026-04-22 10:30:38 +0700

    feat: support slug, katex

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,22 @@ import { reactRouter } from '@react-router/dev/vite'
 import tailwindcss from '@tailwindcss/vite'
 import mdx from '@mdx-js/rollup'
 
+import remarkGfm from 'remark-gfm'
+import remarkMath from 'remark-math'
+
+import rehypeKatex from 'rehype-katex'
 import rehypeMdxImportMedia from 'rehype-mdx-import-media'
+import rehypeSlug from 'rehype-slug'
 
 export default defineConfig({
   plugins: [
     mdx({
       providerImportSource: '~/components/md',
+      remarkPlugins: [remarkGfm, remarkMath],
       rehypePlugins: [
+        rehypeKatex,
         [rehypeMdxImportMedia, { elementAttributeNameCase: 'html' }],
+        rehypeSlug,
       ],
     }),
     reactRouter(),
